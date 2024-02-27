ad2a49c41e3b4e217af35660634e36e57600d154
Marco Pacifico <marco.pacifico@gmail.com>
2024-02-27 10:07:15 -0600

    update hero copy

--- src/components/hero-section-tailwind.tsx
+++ src/components/hero-section-tailwind.tsx
@@ -132,16 +132,16 @@ export default function HeroSectionTailwind() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-[var(--color-brand-900)] ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{" "}
+              Limited time only {" "}
               <a href="#" className="font-semibold text-[var(--color-input)]">
                 <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
+                Special offer <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-[var(--color-brand-950)] sm:text-6xl">
-              Data to enrich your online business
+              Sample hero section for your storefront
             </h1>
             <p className="mt-6 text-lg leading-8 text-[var(--color-brand-900)]">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
