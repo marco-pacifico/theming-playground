f96647fdd15afa36fad91e8295e70aa0013ba301
Marco Pacifico <marco.pacifico@gmail.com>
2024-02-27 13:19:55 -0600

    move pointer events none to different div

--- src/components/hero-section-tailwind.tsx
+++ src/components/hero-section-tailwind.tsx
@@ -116,9 +116,9 @@ export default function HeroSectionTailwind() {
         </Dialog>
       </header>
 
-      <div className="relative isolate px-6 pt-14 lg:px-8 pointer-events-none">
+      <div className="relative isolate px-6 pt-14 lg:px-8 ">
         <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 pointer-events-none"
           aria-hidden="true"
         >
           <div
