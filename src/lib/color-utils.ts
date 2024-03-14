b8c11b8cdf055c5a718e8c73194fcf8d64d40cd2
Marco Pacifico <marco.pacifico@gmail.com>
2024-03-14 17:25:42 -0400

    function for getting matching neutral

--- src/lib/color-utils.ts
+++ src/lib/color-utils.ts
@@ -343,6 +343,76 @@ function findInputColorShadeIndexBasedOnLightnessDifference(
   return closestShadeIndex;
 }
 
+// GET MATHCING NEUTRAL COLOR FROM REFERENCE COLOR SCALES
+export function getMatchingNeutral(closestColor: ClosestColor) {
+  const colorSystem = closestColor.referenceColorSystem;
+  if (colorSystem === "radix") {
+    switch (closestColor.hueName) {
+      case "tomato":
+      case "red":
+      case "ruby":
+      case "crimson":
+      case "pink":
+      case "plum":
+      case "purple":
+      case "violet":
+        return "mauve";
+      case "iris":
+      case "indigo":
+      case "blue":
+      case "sky":
+      case "cyan":
+        return "slate";
+      case "teal":
+      case "jade":
+      case "mint":
+      case "green":
+        return "sage";
+      case "grass":
+      case "lime":
+        return "olive";
+      case "yellow":
+      case "amber":
+      case "orange":
+      case "brown":
+      case "gold":
+      case "bronze":
+        return "sand";
+      case "gray":
+        return "gray";
+    }
+  } else if (colorSystem === "tailwind") {
+    switch (closestColor.hueName) {
+      case "red":
+      case "rose":
+      case "pink":
+      case "fuchsia":
+      case "purple":
+      case "violet":
+        return "mauve";
+      case "indigo":
+      case "blue":
+      case "sky":
+      case "cyan":
+        return "slate";
+      case "teal":
+      case "emerald":
+      case "green":
+        return "sage";
+      case "lime":
+        return "olive";
+      case "yellow":
+      case "amber":
+      case "orange":
+        return "sand";
+      case "gray":
+        return "gray";
+    }
+  }
+  return "gray";
+};
+
+
 // DISPLAY PURPOSES
 // Get the APCA contrast ratio between two colors
 export function getAPCA(
