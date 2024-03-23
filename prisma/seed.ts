1c16eebba4f1bc7b8e10956ef5e6c5366bfc6bd1
Marco Pacifico <marco.pacifico@gmail.com>
2024-03-23 17:16:28 -0400

    seeded db with admin user

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client'
+import { env } from 'node:process';
+const bcrypt = require('bcrypt');
+
+const prisma = new PrismaClient()
+
+async function main() {
+
+ const hashedPassword = await bcrypt.hash(env.ADMIN_PW, 10);
+  const user = await prisma.user.create({
+    data: {
+      name: 'Marco',
+      email: env.ADMIN_EMAIL as string,
+      password: hashedPassword
+    },
+  })
+  console.log(user)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
