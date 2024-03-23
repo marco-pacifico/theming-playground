846745799f0ba175e35adfda853a188a5caebf9a
Marco Pacifico <marco.pacifico@gmail.com>
2024-03-23 17:29:06 -0400

    seeded with a test theme

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,7 +11,16 @@ async function main() {
     data: {
       name: 'Marco',
       email: env.ADMIN_EMAIL as string,
-      password: hashedPassword
+      password: hashedPassword,
+      themes: {
+        create: {
+          name: "Test Theme",
+          brandColor: "#FF0000",
+          neutralColor: "Mauve",
+          radiusMode: "full",
+          headingFont: "Roobert",
+        },
+      },
     },
   })
   console.log(user)
