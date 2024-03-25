145c849d09944be5062ce531be22e1cad7de4d21
Marco Pacifico <marco.pacifico@gmail.com>
2024-03-25 09:30:02 -0400

    only show saved themes if signed in

---
+++ src/app/saved/saved-thems.tsx
@@ -0,0 +1,13 @@
+export default function SavedThemes() {
+  return (
+    <>
+      <section className="mb-12 flex-1">
+        view sameple ui with theme vars applied
+      </section>
+      <aside className="flex flex-1 flex-col border-b border-neutral-200 bg-white md:max-w-[512px] md:border-l">
+        - Select a theme - View theme details - Option to update theme (if
+        changes are made) - Option to delete theme
+      </aside>
+    </>
+  );
+}
